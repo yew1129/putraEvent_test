40524118187a87f2caa61be41ff21199225d6d0d
yew1129 <132594416+yew1129@users.noreply.github.com>
2023-12-31 18:18:12 +0800

    extract and display  data

--- app/src/main/java/com/example/myapptest/EventRVAdapter.java
+++ app/src/main/java/com/example/myapptest/EventRVAdapter.java
@@ -1,27 +1,25 @@
 package com.example.myapptest;
-import com.bumptech.glide.Glide;
-import com.squareup.picasso.Picasso;
-import android.content.Intent;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
+
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 public class EventRVAdapter extends RecyclerView.Adapter<EventRVAdapter.ViewHolder> {
-    // creating variables for our ArrayList and context
+
     private ArrayList<event> eventArrayList;
     private Context context;
 
-    // creating constructor for our adapter class
     public EventRVAdapter(ArrayList<event> eventArrayList, Context context) {
         this.eventArrayList = eventArrayList;
         this.context = context;
@@ -29,25 +27,20 @@ public class EventRVAdapter extends RecyclerView.Adapter<EventRVAdapter.ViewHold
 
     @NonNull
     @Override
-    public EventRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // passing our layout file for displaying our card item
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.event_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // setting data to our text views from our modal class.
         event events = eventArrayList.get(position);
         holder.eventTitleTV.setText(events.getTitle());
         holder.eventDateTV.setText(events.getDate());
         holder.eventDescriptionTV.setText(events.getDescription());
-        Picasso.get().setLoggingEnabled(true);
 
         String imageUrl = events.getImage();
-//        String quotedImageUrl = "https://firebasestorage.googleapis.com/v0/b/putraevent-b6349.appspot.com/o/PutraEVENT%2FConference%20on%20Future.png?alt=media&token=ac7d3e0b-2d6d-4313-89c0-fcb3bbc49061";
         Picasso.get()
                 .load(imageUrl)
-//                .placeholder(R.drawable.resource_default) // Add your placeholder image resource
                 .into(holder.eventImageView);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -55,8 +48,19 @@ public class EventRVAdapter extends RecyclerView.Adapter<EventRVAdapter.ViewHold
             public void onClick(View v) {
                 // Handle the click event, navigate to another page
                 Intent intent = new Intent(context, view_specific_event.class);
-                // Pass any additional data to the new activity if needed
-                // intent.putExtra("key", value);
+
+                // Pass the extracted data into view_specific_event
+                intent.putExtra("eventTitle", events.getTitle());
+                intent.putExtra("eventDate", events.getDate());
+                intent.putExtra("eventDescription", events.getDescription());
+                intent.putExtra("eventImageUrl", events.getImage());
+                intent.putExtra("eventVenue", events.getVenue());
+                intent.putExtra("eventSpeaker", events.getSpeaker_name());
+                intent.putExtra("eventOrganizer", events.getOrganizer());
+                intent.putExtra("eventStartTime", events.getStart_time());
+                intent.putExtra("eventEndTime", events.getEnd_time());
+
+                // Start the activity
                 context.startActivity(intent);
             }
         });
@@ -64,12 +68,10 @@ public class EventRVAdapter extends RecyclerView.Adapter<EventRVAdapter.ViewHold
 
     @Override
     public int getItemCount() {
-        // returning the size of our array list.
         return eventArrayList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
-        // creating variables for our text views.
+    static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView eventTitleTV;
         private final TextView eventDateTV;
         private final TextView eventDescriptionTV;
@@ -77,12 +79,10 @@ public class EventRVAdapter extends RecyclerView.Adapter<EventRVAdapter.ViewHold
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            // initializing our text views.
             eventTitleTV = itemView.findViewById(R.id.idTVTitle);
             eventDateTV = itemView.findViewById(R.id.idTVDate);
             eventDescriptionTV = itemView.findViewById(R.id.idTVDescription);
             eventImageView = itemView.findViewById(R.id.idEventImageView);
-
         }
     }
 }
